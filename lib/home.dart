f459d58fdbb4b8e9e7ed38f7e6e610ace67cb265
yum <yamnaisrailkhan@gmail.com>
2024-06-06 01:15:21 +0500

    Complete Game logic updated

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+
+import 'package:bubbletrouble/ball.dart';
 import 'package:bubbletrouble/button.dart';
+import 'package:bubbletrouble/missile.dart';
+import 'package:bubbletrouble/player.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -7,23 +12,174 @@ class Home extends StatefulWidget {
   @override
   State<Home> createState() => _HomeState();
 }
+enum direction {LEFT, RIGHT}
 
 class _HomeState extends State<Home> {
-  double playerX = 0;
+  static double playerX = 0;
 
+  //missile variables
+  double missileX= playerX;
+  double missileY= 1;
+  double missileHeight = 10;
+  bool midshot= false;
+
+  //ball variables
+  double ballX= 0.5;
+  double ballY= 1;
+  var ballDirection = direction.LEFT;
+
+
+
+  void startGame(){
+
+    double time= 0;
+    double height= 0;
+    double velocity = 60; //how strong jump is
+
+    Timer.periodic(Duration(milliseconds: 10), (timer) {
+
+      //quadratic eq that models a bounce (upside down parabola)
+      height = -5 * time * time + velocity * time;
+
+      //if the ball reaches the ground, reset the jump
+      if(height < 0){
+        time= 0;
+      }
+
+      //update the new ball position
+      setState(() {
+      ballY = heightToCoordinate(height);
+      });
+
+//if ball hits left wall then change the direction to right
+      if( ballX - 0.005 < -1){
+        ballDirection = direction.RIGHT;
+      }
+      //if ball hits right wall then change the direction to left
+
+      else if(ballX + 0.005 > 1){
+        ballDirection = direction.LEFT;
+      }
+
+//move the ball in correct direction
+    if(ballDirection ==  direction.LEFT){
+        setState(() {
+          ballX -= 0.005;
+        });
+      }
+      else if( ballDirection == direction.RIGHT){
+        setState(() {
+          ballX += 0.005;
+        });
+      }
+
+      //check if ball hits the player
+
+      if(playerDies()){
+        timer.cancel();
+        _showDialog();
+      }
+
+      time+=0.1;
+
+
+
+    });
+  }
+void _showDialog(){
+    showDialog(context: context,
+        builder: (BuildContext context){
+      return AlertDialog(
+        backgroundColor: Colors.grey[700],
+        title: Text("You Dead Bro!", style: TextStyle(color: Colors.white),),
+      );
+        });
+}
   void moveLeft() {
+
     setState(() {
-      playerX -= 0.1;
+
+      missileX= playerX;
+
+      if (playerX - 0.1 < -1) {
+        //Do nothing
+      } else {
+        playerX -= 0.1;
+      }
+
+
+      if(!midshot){
+        missileX= playerX;
+      }
     });
   }
 
   void moveRight() {
     setState(() {
-      playerX += 0.1;
+
+      if (playerX + 0.1 > 1) {
+        //Do nothing
+      } else {
+        playerX += 0.1;
+      }
+      missileX= playerX;
     });
   }
 
   void fireMissile() {
+    if(midshot == false){
+      Timer.periodic(Duration(milliseconds: 20), (timer){
+
+        // shot fired
+        midshot= true;
+
+        //midshot grows it reaches till the top of screen
+        setState(() {
+          missileHeight += 10;
+        });
+
+        //stop missile when it reached top of screen
+        if(missileHeight> MediaQuery.of(context).size.height * 3/ 4){
+          resetMissile();
+
+          timer.cancel();
+        }
+
+        //check if missile has hit the ball
+        if(ballY > heightToCoordinate(missileHeight) && ballX == missileX.abs() < 0.03)
+        {
+          resetMissile();
+          ballX = 5;
+          timer.cancel();
+        }
+
+      });
+
+    }
+  }
+
+  double heightToCoordinate(double height){
+    double totalHeight = MediaQuery.of(context).size.height * 3 /4;
+    double position = 1-2 * height / totalHeight;
+    return position;
+  }
+
+  void resetMissile(){
+    missileX= playerX;
+    missileHeight= 10;
+    midshot= false;
+  }
+
+  bool playerDies(){
+    // if the ball touches the player
+// ball and player positions are same
+
+  if((ballX - playerX).abs() < 0.05 && ballY > 0.95){
+    return true;
+  }
+  else{
+    return false;
+  }
 
   }
 
@@ -38,16 +194,10 @@ class _HomeState extends State<Home> {
               child: Center(
                 child: Stack(
                   children: [
-                    ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: Container(
-                          alignment: Alignment(playerX, 1),
-                          child: Container(
-                            color: Colors.deepPurple,
-                            height: 50,
-                            width: 50,
-                          ),
-                        ))
+                    MyBall(ballX, ballY),
+                    MyMissile(missileX, missileHeight),
+                    Player(playerX),
+
                   ],
                 ),
               ),
@@ -58,6 +208,7 @@ class _HomeState extends State<Home> {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
+              MyButton(Icons.play_arrow, startGame),
               MyButton(Icons.arrow_back, moveLeft),
               MyButton(Icons.arrow_upward, fireMissile),
               MyButton(Icons.arrow_forward, moveRight),
